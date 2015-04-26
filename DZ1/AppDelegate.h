760ace00225a6914df139281cbe05a7accdbcd79
Anton_Mednikov <Anton_Mednikov@iMac-mac.local>
2015-04-26 20:43:28 +0300

    Initial Commit

---
+++ DZ1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DZ1
+//
+//  Created by Anton_Mednikov on 26.04.15.
+//  Copyright (c) 2015 Anton Mednikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DZ1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DZ1
+//
+//  Created by Anton_Mednikov on 26.04.15.
+//  Copyright (c) 2015 Anton Mednikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
